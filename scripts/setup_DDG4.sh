a24d432af95113f34d45d581fd829ee907e4877d
clementhelsens <clement.helsens@cern.ch>
2014-08-01 10:27:47 +0200

    remove the G4 export as they are now made from the afs setup.sh

--- scripts/setup_DDG4.sh
+++ scripts/setup_DDG4.sh
@@ -1,11 +1,6 @@
-export MYPATH=/afs/cern.ch/user/s/solans/FCC/fccsw-hh
-
 export DD4hepINSTALL=`dirname $DD4hepLibs`
 export CLHEP_DIR=`dirname $CLHEPlibs`
 export G4INSTALL=`dirname $Geant4libs`
-export G4LEDATA=${G4INSTALL}/share/Geant4-9.6.3/data/G4EMLOW6.32
-export G4LEVELGAMMADATA=${G4INSTALL}/share/Geant4-9.6.3/data/PhotonEvaporation2.3
-export G4SAIDXSDATA=${G4INSTALL}/share/Geant4-9.6.3/data/G4SAIDDATA1.1  
-export PYTHONPATH=$MYPATH/scripts:${DD4hepINSTALL}/python/DDG4/python:$PYTHONPATH
-export LD_LIBRARY_PATH=$MYPATH/install/lib:$LD_LIBRARY_PATH
-export PATH=$MYPATH/install/bin:$PATH
+export PYTHONPATH=$MYLOCALDIR/scripts:${DD4hepINSTALL}/python/DDG4/python:$PYTHONPATH
+export LD_LIBRARY_PATH=$MYLOCALDIR/install/lib:$LD_LIBRARY_PATH
+export PATH=$MYLOCALDIR/install/bin:$PATH

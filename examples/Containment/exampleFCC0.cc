3a15f8201fd49285403495e8b8b3b7a41c08c9d2
clementhelsens <clement.helsens@cern.ch>
2014-08-14 16:37:22 +0200

    replace exampleED.cc with exampleFCC0.cc

---
+++ examples/Containment/exampleFCC0.cc
@@ -0,0 +1,177 @@
+//
+// ********************************************************************
+// * License and Disclaimer                                           *
+// *                                                                  *
+// * The  Geant4 software  is  copyright of the Copyright Holders  of *
+// * the Geant4 Collaboration.  It is provided  under  the terms  and *
+// * conditions of the Geant4 Software License,  included in the file *
+// * LICENSE and available at  http://cern.ch/geant4/license .  These *
+// * include a list of copyright holders.                             *
+// *                                                                  *
+// * Neither the authors of this software system, nor their employing *
+// * institutes,nor the agencies providing financial support for this *
+// * work  make  any representation or  warranty, express or implied, *
+// * regarding  this  software system or assume any liability for its *
+// * use.  Please see the license in the file  LICENSE  and URL above *
+// * for the full disclaimer and the limitation of liability.         *
+// *                                                                  *
+// * This  code  implementation is the result of  the  scientific and *
+// * technical work of the GEANT4 collaboration.                      *
+// * By using,  copying,  modifying or  distributing the software (or *
+// * any work based  on the software)  you  agree  to acknowledge its *
+// * use  in  resulting  scientific  publications,  and indicate your *
+// * acceptance of all terms of the Geant4 Software license.          *
+// ********************************************************************
+//
+// $Id$
+//
+/// \file exampleFCC0.cc
+/// \brief Main program of the FCC0 example
+
+#include "FCC0DetectorConstruction.hh"
+#include "FCC0ActionInitialization.hh"
+
+#ifdef G4MULTITHREADED
+#include "G4MTRunManager.hh"
+#else
+#include "G4RunManager.hh"
+#endif
+
+#include "G4UImanager.hh"
+#include "G4PhysListFactory.hh"
+
+#ifdef G4VIS_USE
+#include "G4VisExecutive.hh"
+#endif
+
+#ifdef G4UI_USE
+#include "G4UIExecutive.hh"
+#endif
+
+#include "Randomize.hh"
+
+//....oooOO0OOooo........oooOO0OOooo........oooOO0OOooo........oooOO0OOooo......
+
+namespace {
+  void PrintUsage() {
+    G4cerr << " Usage: " << G4endl;
+    G4cerr << " exampleB4a [-m macro ] [-p physList ] [-u UIsession] [-t threads]" << G4endl;
+    G4cerr << G4endl;
+  }
+}
+
+//....oooOO0OOooo........oooOO0OOooo........oooOO0OOooo........oooOO0OOooo......
+
+int main(int argc,char** argv)
+{
+  // Evaluate arguments
+  //
+  if ( argc > 7 ) {
+    PrintUsage();
+    return 1;
+  }
+  
+  G4String macro;
+  G4String session;
+  G4String physicsListName;
+  G4int threads;
+  for ( G4int i=1; i<argc; i=i+2 ) {
+    if      ( G4String(argv[i]) == "-m" ) macro = argv[i+1];
+    else if ( G4String(argv[i]) == "-u" ) session = argv[i+1];
+    else if ( G4String(argv[i]) == "-p" ) physicsListName = argv[i+1];
+    else if ( G4String(argv[i]) == "-t" ) threads = atoi(argv[i+1]);
+
+    else {
+      PrintUsage();
+      return 1;
+    }
+  }  
+  
+
+
+  // Choose the Random engine
+  //
+  G4Random::setTheEngine(new CLHEP::RanecuEngine);
+  
+  // Construct the run manager
+  //
+#ifdef G4MULTITHREADED
+  G4MTRunManager* runManager = new G4MTRunManager;
+  runManager->SetNumberOfThreads(threads);
+#else
+  G4RunManager* runManager = new G4RunManager;
+#endif
+
+  // Set mandatory initialization classes
+  //
+  // Detector construction
+  runManager->SetUserInitialization(new FCC0DetectorConstruction());
+
+  // Physics list
+  if ( physicsListName.size() == 0 ) physicsListName = "QGSP_BERT";
+  G4PhysListFactory physListFactory;
+  if ( ! physListFactory.IsReferencePhysList(physicsListName)) {
+    G4cerr << " Physics list " << physicsListName 
+           << " is not defined." << G4endl;
+    return 1;
+  }  
+  
+  G4VModularPhysicsList* physicsList 
+    = physListFactory.GetReferencePhysList(physicsListName);
+  physicsList->SetVerboseLevel(1);
+  runManager->SetUserInitialization(physicsList);
+    
+  // User action initialization
+  runManager->SetUserInitialization(new FCC0ActionInitialization());
+
+  // Initialize G4 kernel
+  //
+  runManager->Initialize();
+  
+#ifdef G4VIS_USE
+  // Initialize visualization
+  G4VisManager* visManager = new G4VisExecutive;
+  // G4VisExecutive can take a verbosity argument - see /vis/verbose guidance.
+  // G4VisManager* visManager = new G4VisExecutive("Quiet");
+  visManager->Initialize();
+#endif
+
+  // Get the pointer to the User Interface manager
+  G4UImanager* UImanager = G4UImanager::GetUIpointer();
+
+  if ( macro.size() ) {
+    // batch mode
+    G4String command = "/control/execute ";
+    UImanager->ApplyCommand(command+macro);
+  }
+  else {
+    // interactive mode : define UI session
+#ifdef G4UI_USE
+    G4UIExecutive* ui = new G4UIExecutive(argc, argv, session);
+#ifdef G4VIS_USE
+    UImanager->ApplyCommand("/control/execute init_vis.mac"); 
+#else
+    UImanager->ApplyCommand("/control/execute init.mac"); 
+#endif
+    if (ui->IsGUI()) {
+       UImanager->ApplyCommand("/control/execute icons.mac");
+    }
+    ui->SessionStart();
+    delete ui;
+#endif
+  }
+
+  // Job termination
+  // Free the store: user actions, physics_list and detector_description are
+  // owned and deleted by the run manager, so they should not be deleted 
+  // in the main() program !
+  
+#ifdef G4VIS_USE
+  delete visManager;
+#endif
+  delete runManager;
+
+  return 0;
+}
+
+//....oooOO0OOooo........oooOO0OOooo........oooOO0OOooo........oooOO0OOooo.....

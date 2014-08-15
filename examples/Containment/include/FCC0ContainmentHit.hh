6549fcf08ead5af46f3b9f7addd41ada87235fed
clementhelsens <clement.helsens@cern.ch>
2014-08-15 11:28:40 +0200

    add FCC0ContainmentHit

---
+++ examples/Containment/include/FCC0ContainmentHit.hh
@@ -0,0 +1,100 @@
+//
+// ********************************************************************
+// * License and Disclaimer                                           *
+// *                                                                  *
+// * The  Geant4 software  is  copyright of the Copyright Holders  of *
+// * the Geant4 Collaboration.  It is provided  under  the terms  and *
+// * conditions of the Geant4 Software License,  included in the file *
+// * LICENSE and available at  http://cern.ch/geant4/license .  These *
+// * include a list of copyright holders.                             *
+// *                                                                  *
+// * Neither the authors of this software system, nor their employing *
+// * institutes,nor the agencies providing financial support for this *
+// * work  make  any representation or  warranty, express or implied, *
+// * regarding  this  software system or assume any liability for its *
+// * use.  Please see the license in the file  LICENSE  and URL above *
+// * for the full disclaimer and the limitation of liability.         *
+// *                                                                  *
+// * This  code  implementation is the result of  the  scientific and *
+// * technical work of the GEANT4 collaboration.                      *
+// * By using,  copying,  modifying or  distributing the software (or *
+// * any work based  on the software)  you  agree  to acknowledge its *
+// * use  in  resulting  scientific  publications,  and indicate your *
+// * acceptance of all terms of the Geant4 Software license.          *
+// ********************************************************************
+//
+// $Id$
+//
+/// \file FCC0ContainmentHit.hh
+/// \brief Definition of the FCC0ContainmentHit class
+//
+
+#ifndef FCC0ContainmentHit_h
+#define FCC0ContainmentHit_h 1
+
+#include "G4VHit.hh"
+#include "G4THitsCollection.hh"
+#include "G4Allocator.hh"
+#include "G4ThreeVector.hh"
+#include "G4Types.hh"
+
+
+class FCC0ContainmentHit : public G4VHit
+{
+public:
+  FCC0ContainmentHit();
+  virtual ~FCC0ContainmentHit();
+  FCC0ContainmentHit(const FCC0ContainmentHit& right);
+  const FCC0ContainmentHit& operator=(const FCC0ContainmentHit& right);
+  int operator==(const FCC0ContainmentHit& right) const;
+  
+  inline void* operator new(size_t);
+  inline void  operator delete(void* hit);
+  
+  virtual void Print();
+  
+  // add setter/getter methods
+  void SetLayerNumber(G4int number) { fLayerNumber = number; }
+  void AddEdep(G4double edep)       { fEdep += edep; }
+  void SetPosition(G4ThreeVector position) { fPosition = position; }
+  void SetMomentum(G4ThreeVector momentum) { fMomentum = momentum;}
+  void SetKinEnergy(G4double kinEnergy)    {fKinEnergy = kinEnergy;}
+  void SetGlobalTime(G4double globalTime)  {fGlobalTime = globalTime;}
+
+  
+  G4int    GetLayerNumber() const { return fLayerNumber;}
+  G4double GetEdep() const        { return fEdep; }
+  G4ThreeVector  GetPosition() const    { return fPosition; } 
+  G4ThreeVector  GetMomentum() const    { return fMomentum; } 
+  G4double GetKinEnergy() const { return fKinEnergy;}
+  G4double GetGlobalTime() const { return fGlobalTime;}
+  
+private:
+  // add data members
+  G4int     fLayerNumber;
+  G4double  fEdep;
+  G4ThreeVector  fPosition;
+  G4double fGlobalTime;
+  G4double fKinEnergy;
+  G4ThreeVector fMomentum;
+};
+
+typedef G4THitsCollection<FCC0ContainmentHit> FCC0ContainmentHitsCollection;
+
+extern G4ThreadLocal G4Allocator<FCC0ContainmentHit>* FCC0ContainmentHitAllocator;
+
+inline void* FCC0ContainmentHit::operator new(size_t)
+{
+  if (! FCC0ContainmentHitAllocator)
+        FCC0ContainmentHitAllocator = new G4Allocator<FCC0ContainmentHit>;
+  return (void*)FCC0ContainmentHitAllocator->MallocSingle();
+}
+
+inline void FCC0ContainmentHit::operator delete(void* hit)
+{
+  FCC0ContainmentHitAllocator->FreeSingle((FCC0ContainmentHit*) hit);
+}
+
+#endif
+
+
---
+++ examples/Containment/src/FCC0ContainmentHit.cc
@@ -0,0 +1,95 @@
+//
+// ********************************************************************
+// * License and Disclaimer                                           *
+// *                                                                  *
+// * The  Geant4 software  is  copyright of the Copyright Holders  of *
+// * the Geant4 Collaboration.  It is provided  under  the terms  and *
+// * conditions of the Geant4 Software License,  included in the file *
+// * LICENSE and available at  http://cern.ch/geant4/license .  These *
+// * include a list of copyright holders.                             *
+// *                                                                  *
+// * Neither the authors of this software system, nor their employing *
+// * institutes,nor the agencies providing financial support for this *
+// * work  make  any representation or  warranty, express or implied, *
+// * regarding  this  software system or assume any liability for its *
+// * use.  Please see the license in the file  LICENSE  and URL above *
+// * for the full disclaimer and the limitation of liability.         *
+// *                                                                  *
+// * This  code  implementation is the result of  the  scientific and *
+// * technical work of the GEANT4 collaboration.                      *
+// * By using,  copying,  modifying or  distributing the software (or *
+// * any work based  on the software)  you  agree  to acknowledge its *
+// * use  in  resulting  scientific  publications,  and indicate your *
+// * acceptance of all terms of the Geant4 Software license.          *
+// ********************************************************************
+//
+// $Id$
+//
+/// \file FCC0ContainmentHit.cc
+/// \brief Implementation of the FCC0ContainmentHit class
+//
+
+#include "FCC0ContainmentHit.hh"
+
+#include <G4UnitsTable.hh>
+#include "G4SystemOfUnits.hh"
+#include <iomanip>
+
+//....oooOO0OOooo........oooOO0OOooo........oooOO0OOooo........oooOO0OOooo......
+
+//            G4Allocator<FCC0ContainmentHit>* FCC0ContainmentHitAllocator = 0;
+
+G4ThreadLocal G4Allocator<FCC0ContainmentHit>* FCC0ContainmentHitAllocator = 0;
+
+//....oooOO0OOooo........oooOO0OOooo........oooOO0OOooo........oooOO0OOooo......
+
+FCC0ContainmentHit::FCC0ContainmentHit()
+ : G4VHit(),
+   fLayerNumber(-1),
+   fEdep(0.),
+   fPosition()
+
+{}
+
+//....oooOO0OOooo........oooOO0OOooo........oooOO0OOooo........oooOO0OOooo......
+
+FCC0ContainmentHit::~FCC0ContainmentHit()
+{}
+
+//....oooOO0OOooo........oooOO0OOooo........oooOO0OOooo........oooOO0OOooo......
+
+FCC0ContainmentHit::FCC0ContainmentHit(const FCC0ContainmentHit& /*right*/)
+ : G4VHit() 
+{}
+
+//....oooOO0OOooo........oooOO0OOooo........oooOO0OOooo........oooOO0OOooo......
+
+const FCC0ContainmentHit& 
+FCC0ContainmentHit::operator=(const FCC0ContainmentHit& /*right*/)
+{
+  return *this;
+}
+
+//....oooOO0OOooo........oooOO0OOooo........oooOO0OOooo........oooOO0OOooo......
+
+int FCC0ContainmentHit::operator==(const FCC0ContainmentHit& /*right*/) const
+{
+  return 0;
+}
+
+//....oooOO0OOooo........oooOO0OOooo........oooOO0OOooo........oooOO0OOooo......
+
+void FCC0ContainmentHit::Print()
+{
+  //  if ( fEdep > 0. ) {
+    G4cout << "Containment hit in layer: " << fLayerNumber 
+           << "  kinEnergy: " << std::setw(7) << G4BestUnit(fKinEnergy,"Energy")
+	   << "   position [mm]: " <<  fPosition/mm
+ 	   << "   momentum : " <<  fMomentum
+	   << G4endl;
+    //}          
+}
+
+//....oooOO0OOooo........oooOO0OOooo........oooOO0OOooo........oooOO0OOooo......
+
+
